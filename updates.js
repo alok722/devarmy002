eb99f2839170ef08f98b4c85c0d5e834e63f86eb
Ayush909 <kushwahaayu9@gmail.com>
2020-05-15 13:30:33 +0530

    added thumbnail

--- updates.js
+++ updates.js
@@ -1,7 +1,7 @@
 const containermain = document.getElementById('container-main')
 
 const fetchNews = async ()=>{
-    const fetchNews = await fetch('http://newsapi.org/v2/top-headlines?country=in&from=2020-05-14&sortBy=popularity&apiKey=6f1082065f47444fb604f6801b3b7ffa&category=technology')
+    const fetchNews = await fetch('https://newsapi.org/v2/top-headlines?country=in&from=2020-05-14&sortBy=popularity&apiKey=6f1082065f47444fb604f6801b3b7ffa&category=technology')
     const res = await fetchNews.json();
     createSingleNews(res.articles)
 } 
@@ -16,7 +16,14 @@ function createSingleNews(articles){
             const singlenews = document.createElement('div')
             const authortext = article.author ? article.author : "Not available" 
             singlenews.classList.add("single-news")
-            singlenews.innerHTML = `<h3><a target="_blank" href='${article.url}'>${article.title}</a></h3><div class='news-details'><p>Source: ${article.source.name}</p><p>Author: ${authortext}</p></div><p>${article.description}</p>`
+            singlenews.innerHTML = `<div class="news-thumbnail">
+                                        <img src="${article.urlToImage}" >
+                                     </div>
+                                    <div class="news-details">
+                                        <h3><a target="_blank" href='${article.url}'>${article.title}</a></h3>
+                                        <div class='article-details'><p>Source: ${article.source.name}</p><p>Author: ${authortext}</p></div>
+                                        <p>${article.description}</p>
+                                    </div>`
             containermain.appendChild(singlenews)
         }       
     });
